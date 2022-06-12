cfa34e9e6941384f1b51a0a90d502b335b3c9565
kelvinachiando <kelvinostin22gmail.com>
2022-06-12 21:59:16 +0300

    commit tree 63de8690294f4c592619b3ec1589b9862ac92b14

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin (array){
+    function test(element){
+        return (element.result==="W")
+    }
+    if (array.find (test)){
+        return array.find (test).year
+    }
+}
